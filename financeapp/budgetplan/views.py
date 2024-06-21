3853922bb62aa79d0e9d00f9403d9e6816896657
anidxn <anidxn215@gmail.com>
2024-06-21 09:11:08 +0530

    changes

--- financeapp/budgetplan/views.py
+++ financeapp/budgetplan/views.py
@@ -15,6 +15,8 @@ from django.db.models import Sum, F
 from django.db.models.functions import TruncMonth
 from datetime import datetime, timedelta
 
+from django.http import JsonResponse
+
 #we create a viewset class by extending viewsets.ModelViewSet which provides default create(),retrieve(),list(),update(),partialupdate(),destroy() actions
 class CatViewSet(viewsets.ModelViewSet):
     #queryset and serializer_class is an inbuilt data object so don't change the name
@@ -55,24 +57,25 @@ class ActViewSet(viewsets.ModelViewSet):
 
 
 #-------------------------------------------------------------
-#               SEARCH by Date Range
+#               SEARCH by filtering
 #-------------------------------------------------------------
 """
 create a view class by extending viewsets.ListAPIView which is specialized view that only provides read-only access 
 to a list of objects. It supports GET requests to list a queryset.
 """
 
-
 class ActivityAPIView(generics.ListAPIView):
     serializer_class = ActivityCategoryLinkSerializer
     
     def get_queryset(self):
-        queryset = Activity.objects.all()
-        start_date  = self.request.query_params.get('start_date', None)
+        queryset = Activity.objects.all()   # get all activities from DB
+
+        # check for appropriate query parameters
+        # for report
+        start_date  = self.request.query_params.get('start_date', None) 
         end_date    = self.request.query_params.get('end_date', None)
         if start_date and end_date:
             queryset = queryset.filter(a_date__range=[start_date, end_date])
-        return queryset
     
     """
     http://localhost:8085/finapi/activityfilter/?start_date=2023-01-01&end_date=2023-12-31
@@ -82,26 +85,93 @@ class ActivityAPIView(generics.ListAPIView):
 #     Aggregate category wise total expense & SEARCH category by Tags / min budget / max budget
 #-----------------------------------------------------------------------------------------------------
 class CategoryAPIView(generics.ListAPIView):
-    # queryset = Category.objects.annotate(total_expenditure=Sum('activity__expense'))
+    # queryset = Category.objects.annotate(total_expenditure=Sum('activity__expense'))  # total_expenditure is the custom field name
     serializer_class = CategoryExpenditureSerializer
 
     def get_queryset(self):
-        queryset = Category.objects.annotate(total_expenditure=Sum('activity__expense')) # total_expenditure is the custom field name
 
-        # Get query parameters
-        cat_tag = self.request.query_params.get('cat_tag', None)
-        min_budget = self.request.query_params.get('min_budget', None)
-        max_budget = self.request.query_params.get('max_budget', None)
+        # Get query parameters 
+        num_months  = self.request.query_params.get('num_months', None)
+        if num_months:
+            num_months = int(num_months)
+            """
+                return month-wise total expenditure group by cat_tag and for the last 6 months.
+            """
 
-        # Filter queryset based on query parameters
-        if cat_tag:
-            queryset = queryset.filter(cat_tags=cat_tag)
-        if min_budget:
-            queryset = queryset.filter(budget__gte=min_budget)
-        if max_budget:
-            queryset = queryset.filter(budget__lte=max_budget)
+            # Calculate the date for Num (6) months ago
+            num_months_ago = datetime.today() - timedelta(days = num_months * 30)   # 6 * 30
 
+            # Filter ACTIVITIES in the last Num (6) months
+            queryset = Activity.objects.filter(a_date__gte=num_months_ago)
+
+            # Annotate month and group by month and cat_tag & get total_expense
+            queryset = queryset.annotate(
+                month=TruncMonth('a_date')
+            ).values(
+                'month', 'a_cat__cat_tags'
+            ).annotate(
+                total_expenditure=Sum('expense')
+            ).order_by('month', 'a_cat__cat_tags')
+
+        else:
+
+            # Get Category wise total_expense
+            queryset = Category.objects.annotate(total_expenditure=Sum('activity__expense')) # total_expenditure is the custom field name
+            
+            # Get query parameters 
+            cat_tag = self.request.query_params.get('cat_tag', None)
+            min_budget = self.request.query_params.get('min_budget', None)
+            max_budget = self.request.query_params.get('max_budget', None)
+
+            # Filter queryset based on query parameters
+            if cat_tag:
+                queryset = queryset.filter(cat_tags=cat_tag)
+            if min_budget:
+                queryset = queryset.filter(budget__gte=min_budget)
+            if max_budget:
+                queryset = queryset.filter(budget__lte=max_budget)
+
+
+        # return the final result after filter
         return queryset
+    
+    # --------- overriding the list() to output our specific format ------------
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        if request.query_params.get('num_months'):
+            
+
+            # Process the data to group by month and cat_tag
+            result = {}
+            for expenditure in queryset:
+
+                month = expenditure['month'].strftime('%Y-%m')
+                cat_tag = expenditure['a_cat__cat_tags']
+
+                if month not in result:
+                    result[month] = {}
+                if cat_tag not in result[month]:
+                    result[month][cat_tag] = 0
+                result[month][cat_tag] += expenditure['total_expenditure']
+
+            return Response(result)
+        else:
+            # result = {}
+            # for expenditure in queryset:
+
+
+            #     month = expenditure['month'].strftime('%Y-%m')
+            #     cat_tag = expenditure['a_cat__cat_tags']
+
+            #     if month not in result:
+            #         result[month] = {}
+            #     if cat_tag not in result[month]:
+            #         result[month][cat_tag] = 0
+            #     result[month][cat_tag] += expenditure['total_expenditure']
+
+            return JsonResponse(queryset,safe=False)
+           
+
     
     """
         http://localhost:8085/finapi/categoryexpfilter/
@@ -111,72 +181,10 @@ class CategoryAPIView(generics.ListAPIView):
         http://localhost:8085/finapi/categoryexpfilter/?cat_tag=Need&min_budget=1000&max_budget=5000
     """
     
-    """
-    return month-wise total expenditure group by cat_tag and for the last 6 months.
-    """
-    def list(self, request, *args, **kwargs):
-        # Calculate the date for 6 months ago
-        six_months_ago = datetime.today() - timedelta(days=6*30)
-
-        # Filter activities in the last 6 months
-        activities = Activity.objects.filter(a_date__gte=six_months_ago)
-
-        # Annotate month and group by month and cat_tag
-        expenditures = activities.annotate(
-            month=TruncMonth('a_date')
-        ).values(
-            'month', 'a_cat__cat_tags'
-        ).annotate(
-            total_expenditure=Sum('expense')
-        ).order_by('month', 'a_cat__cat_tags')
-
-        # Process the data to group by month and cat_tag
-        result = {}
-        for expenditure in expenditures:
-            month = expenditure['month'].strftime('%Y-%m')
-            cat_tag = expenditure['a_cat__cat_tags']
-            if month not in result:
-                result[month] = {}
-            if cat_tag not in result[month]:
-                result[month][cat_tag] = 0
-            result[month][cat_tag] += expenditure['total_expenditure']
-
-        return Response(result)
     
-    # XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-    def get_queryset(self):
-        # Calculate the date for 6 months ago
-        six_months_ago = datetime.today() - timedelta(days=6*30)
+    
 
-        # Filter activities in the last 6 months
-        queryset = Activity.objects.filter(a_date__gte=six_months_ago)
-
-        # Annotate month and group by month and cat_tag
-        queryset = queryset.annotate(
-            month=TruncMonth('a_date')
-        ).values(
-            'month', 'a_cat__cat_tags'
-        ).annotate(
-            total_expenditure=Sum('expense')
-        ).order_by('month', 'a_cat__cat_tags')
-
-        return queryset
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-
-        # Process the data to group by month and cat_tag
-        result = {}
-        for expenditure in queryset:
-            month = expenditure['month'].strftime('%Y-%m')
-            cat_tag = expenditure['a_cat__cat_tags']
-            if month not in result:
-                result[month] = {}
-            if cat_tag not in result[month]:
-                result[month][cat_tag] = 0
-            result[month][cat_tag] += expenditure['total_expenditure']
-
-        return Response(result)
+    
 
 
 # # -------------------------------------------------------------------
